5dba491a3feaf7683d3bbb276891f16c7bc968ef
isklikas <isklikas@freemail.gr>
2017-03-27 15:33:54 +0300

    Initial Commit

---
+++ Deluge Remote/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Deluge Remote
+//
+//  Created by Yannis on 27/03/2017.
+//  Copyright © 2017 isklikas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Deluge Remote/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Deluge Remote
+//
+//  Created by Yannis on 27/03/2017.
+//  Copyright © 2017 isklikas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
