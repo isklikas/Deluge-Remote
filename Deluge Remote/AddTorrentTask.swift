4a201c1d650d8e4887ef72849fb68a01d152780c
isklikas <isklikas@freemail.gr>
2017-06-12 14:02:20 +0300

    All add scenarios implemented

--- Deluge Remote/AddTorrentTask.swift
+++ Deluge Remote/AddTorrentTask.swift
@@ -27,8 +27,40 @@ class AddTorrentTask: TaskWithOptions {
         if (self.rTorrent != nil) {
             switch self.rTorrent!.type {
             case "Array":
+                let fileArray: NSArray = self.rTorrent!.taskObject as! NSArray
+                let mutableArray = NSMutableArray(array: fileArray) as Array
+                var output = "["
+                for i in 0 ..< mutableArray.count {
+                    let fileURL = mutableArray[i] as! NSURL
+                    let filename : String = fileURL.path!.components(separatedBy: "/").last!
+                    var filedump: Data = Data()
+                    do {
+                        filedump = try Data(contentsOf: fileURL as URL)
+                    }
+                    catch {
+                    }
+                    let b64encoded: String = filedump.base64EncodedString()
+                    let currentTuple = String(format:"(\"%@\",\"%@\",[ENTER_PYDICT_HERE])", filename, b64encoded)
+                    output += currentTuple
+                    
+                    if i < (mutableArray.count-1) {
+                        output += ","
+                    }
+                }
+                output += "]"
+                self.cmdString = String(format:"tup = \"%@\" \n    client.core.add_torrent_files(tup).addCallback(on_get_status)",output)
                 break;
             case "File":
+                let fileURL : NSURL = self.rTorrent!.taskObject as! NSURL
+                let filename : String = fileURL.path!.components(separatedBy: "/").last!
+                var filedump: Data = Data()
+                do {
+                    filedump = try Data(contentsOf: fileURL as URL)
+                }
+                catch {
+                }
+                let b64encoded: String = filedump.base64EncodedString()
+                self.cmdString = String(format:"filename = \"%@\" \n    filedump = \"%@\" \n    client.core.add_torrent_file(filename, filedump, [ENTER_PYDICT_HERE]).addCallback(on_get_status)",filename,b64encoded)
                 break;
             default:
                 //Magnet Link
